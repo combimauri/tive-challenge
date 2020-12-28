50e27c22b58254c75e9c3753bf9cebebe249dc00
Mauricio Arce <mauricio.arce.mat@gmail.com>
2020-12-28 00:04:54 -0400

    TIVE-1. Deleted unused files.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'tive-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'tive-challenge';
-}
+export class AppComponent {}
